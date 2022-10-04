0f5b3f0515553aaefd4b1c17307fd59dbf13f6f7
hoangphuc <hoangphucnb97@gmail.com>
2022-10-04 15:45:28 +0700

    Airdrop accounts after generate it

--- scripts/0_generate_5_accounts.ts
+++ scripts/0_generate_5_accounts.ts
@@ -1,6 +1,8 @@
 import { Keypair } from "@solana/web3.js";
-import {getAccounts, getContracts, saveAccount} from "./utils";
+import {airdropSol, getAccounts, getConnection, getContracts, saveAccount} from "./utils";
 import bs58 from "bs58";
+import dotenv from "dotenv";
+dotenv.config();
 
 async function generateFiveAccounts() {
     for (let i = 0; i < 5; i++) {
@@ -10,6 +12,18 @@ async function generateFiveAccounts() {
     console.log(`Generate 5 accounts success`);
 }
 
+async function airdropFiveAccounts() {
+    const network = process.env.NETWORK;
+    const connection = await getConnection(network);
+    for (let i = 0; i < 5; i++) {
+        const secretKey = await getAccounts(i);
+        const keypair = Keypair.fromSecretKey(bs58.decode(secretKey));
+        await airdropSol(keypair, connection);
+    }
+    console.log(`Airdrop 5 account success`);
+}
+
 (async function main() {
     await generateFiveAccounts();
+    await airdropFiveAccounts();
 })();
