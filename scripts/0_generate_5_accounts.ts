5a86fb000a5d46d115e0d26ed9939ed5e10ead39
hoangphuc <hoangphucnb97@gmail.com>
2022-10-04 17:08:54 +0700

    Generate new MOVE address

--- scripts/0_generate_5_accounts.ts
+++ scripts/0_generate_5_accounts.ts
@@ -12,18 +12,6 @@ async function generateFiveAccounts() {
     console.log(`Generate 5 accounts success`);
 }
 
-async function airdropFiveAccounts() {
-    const network = process.env.NETWORK;
-    const connection = await getConnection(network);
-    for (let i = 0; i < 5; i++) {
-        const keypair = await getKeypair(i);
-        await airdropSol(keypair, connection);
-        await sleep(10000);
-    }
-    console.log(`Airdrop 5 account success`);
-}
-
 (async function main() {
     await generateFiveAccounts();
-    await airdropFiveAccounts();
 })();
