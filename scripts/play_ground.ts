4644d1eb6bfa877d28a8a5e7d9b28bd6ab95fba2
hoangphuc <hoangphucnb97@gmail.com>
2022-10-04 14:32:32 +0700

    Add process env config to automatically load netowork

--- scripts/play_ground.ts
+++ scripts/play_ground.ts
@@ -1,5 +1,22 @@
-import { Keypair, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { Keypair, Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+import dotenv from "dotenv";
+import * as Cluster from "cluster";
 
+dotenv.config();
+
+async function getConnection(): Promise<Connection> {
+    const network = process.env.NETWORK;
+    const commitment = 'confirmed';
+    let connection: Connection;
+    if (['devnet','testnet','mainnet-beta'].indexOf(network) >= 0) {
+        connection = new Connection(clusterApiUrl(network as any), commitment)
+    } else if (network === 'local') {
+        connection = new Connection('http://127.0.0.1:8899', commitment);
+    } else {
+        throw Error(`Network config errored ${network}`);
+    }
+    return connection;
+}
 
 async function airdropSol(payer: Keypair, connection: Connection) {
     const airdropSignature = await connection.requestAirdrop(
@@ -19,9 +36,10 @@ async function airdropSol(payer: Keypair, connection: Connection) {
     console.log(`Balance of account ${payer.publicKey} is ${balanceAccount}`);
 }
 
+
 (async function main() {
     const payer = Keypair.generate();
-    const connection =  new Connection("http://127.0.0.1:8899", 'confirmed');
+    const connection =  await getConnection();
     console.log(`payer address is ${payer.publicKey}`);
     await airdropSol(payer, connection);
 })()
