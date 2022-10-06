cd40132a1ce794c99558301e5449f75ee33046fc
hoangphuc <hoangphucnb97@gmail.com>
2022-10-06 11:52:53 +0700

    Add test check mint token initiate correctly

--- tests/swap_token.ts
+++ tests/swap_token.ts
@@ -3,6 +3,9 @@ import { Program } from "@project-serum/anchor";
 import { SwapToken } from "../target/types/swap_token";
 import {createMint} from "@solana/spl-token";
 import {airdropSol} from "../utils";
+import base58 from "bs58";
+import base = Mocha.reporters.base;
+import * as assert from "assert";
 
 describe("swap_token", () => {
   const provider = anchor.AnchorProvider.local();
@@ -15,6 +18,7 @@ describe("swap_token", () => {
   it("Is initialized!", async () => {
     // Add your test here.
     const user = anchor.web3.Keypair.generate();
+    const stateAccount = anchor.web3.Keypair.generate();
     await airdropSol(user, provider.connection);
 
     const mint = await createMint(
@@ -25,15 +29,18 @@ describe("swap_token", () => {
         9,
     );
 
-    console.log(`Deploy new token at ${mint.toBase58()}`)
-
     await program.methods.initialize()
         .accounts({
           userPuller: user.publicKey,
           moveToken: mint,
           systemProgram: anchor.web3.SystemProgram.programId,
+          state: stateAccount.publicKey,
         })
-        .signers([user])
+        .signers([user, stateAccount])
         .rpc();
+
+    const state = await program.account.state.fetch(stateAccount.publicKey);
+    const moveToken = state.moveToken;
+    assert.equal(mint.toBase58(), base58.encode(moveToken.toBytes()));
   });
 });
